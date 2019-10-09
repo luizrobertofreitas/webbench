e6ad5a0b7545be7b77b314cc9802a549c3c79486
Luiz Roberto Freitas <luiz.junior@accurate.com.br>
2019-10-09 14:40:08 -0300

    Bench first version

---
+++ ginmain.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+  "net/http"
+  "strconv"
+  //"log"
+  //"fmt"
+)
+
+func main() {
+  router := gin.Default()
+
+  router.GET("/:n", func(c *gin.Context) {
+    n, _ := strconv.ParseUint(c.Param("n"), 10, 64)
+    //log.Println("Err: ", err)
+    res := fact(n)
+    //message := fmt.Sprintf("Factorial of %d is %d", n, res)
+    //log.Println("M: ", message)
+    c.String(http.StatusOK, string(res))
+  })
+
+  router.Run(":8091")
+}
+
+func fact(n uint64) uint64 {
+  if (n == 1) {
+    return 1
+  }
+  return n*fact(n-1)
+}
---
+++ moes-app.py
@@ -0,0 +1,16 @@
+# pip install japronto
+
+from japronto import Application
+
+def factorial(number):
+    return 1 if number == 0 else number*factorial(number-1)
+
+def fac (request):
+    pathParam = request.match_dict
+    n = int(pathParam.get('n'))
+    soma =  factorial(n)
+    return request.Response(text=str(soma))
+
+app = Application()
+app.router.add_route('/{n}',fac)
+app.run(debug=True, port=8090)
